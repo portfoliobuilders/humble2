62f480be86b10d32e4be1a739fde1755364b7a0b
fahad musthafa <fahadmusthafa2k@gmail.com>
2025-04-22 11:46:33 +0530

    fahad 4th commit

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1924527460"
+    serviceWorkerVersion: "1023534921"
   }
 });
